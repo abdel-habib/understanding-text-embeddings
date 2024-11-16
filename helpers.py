c8b393e82969e456f7f20faa35a2707677cfe0b1
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2024-11-17 00:01:53 +0300

    different embedding pooling mechanisms

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import torch 
 
 def show_tokenization(inputs, tokenizer):
     ''' Show tokenization of a returned input. '''
@@ -41,3 +42,54 @@ def search_through_vocab_dict_using_token(tokenizer, token):
     vocab_dict = tokenizer.get_vocab()
 
     return vocab_dict[token] if token in list(vocab_dict.keys()) else 'Token not found in the vocabulary.'
+
+
+def embedding_pooling(embeddings, attention_mask = None, pooling_strategy = 'cls'):
+    ''' Pool the embeddings outputed by the model using the specified pooling strategy.
+
+    The embeddings has a shape of (batch_size, sequence_length, hidden_size). 
+    The attention mask is used in some pooling strategies to ignore the padding tokens. Those text tokens are extracted from the inputs using the 1's in the attention mask, where any 
+    padded values will be given a  0 attention value.
+    
+    Args:
+        embeddings: The embeddings to pool.
+        attention_mask: The attention mask to use to ignore the padding tokens. It is used in some pooling strategies.
+        pooling_strategy: The pooling strategy to use. It can be either 'mean' or 'max'.
+
+    Returns:
+        The pooled embeddings.
+    '''
+    def _validate_attention_mask(attention_mask):
+        if attention_mask is None:
+            raise ValueError('`attention_mask` must be provided for some pooling strategy to extract the attention mask and handle the padded tokens with zero attention.')
+
+    if pooling_strategy == 'cls':
+        return embeddings[:, 0, :] # take the first token of the sequence: [CLS] token, shape: (batch_size, hidden_size)
+
+    elif pooling_strategy == 'eos':
+        # validate if the inputs are given to ensure the attention mask is available
+        _validate_attention_mask(attention_mask)
+        
+        # using the attention mask to ignore the padding tokens and take the last token of the sequence: [EOS] token
+        eos_token_indices = attention_mask.sum(dim=-1) - 1 # the -1 is as the indices start from 0, and if the last token is [EOS], then the index is the length of the sequence - 1
+        return embeddings[torch.arange(embeddings.shape[0], device=embeddings.device), eos_token_indices] # shape: (batch_size, hidden_size)
+        
+    elif pooling_strategy == 'max':
+        _validate_attention_mask(attention_mask)
+        # the unsqueeze is to make the attention mask have the same shape as the embeddings
+        # the multiplication is to zero out the embeddings of the padding tokens
+        max, _ = torch.max(embeddings * attention_mask.unsqueeze(-1), dim=1) 
+        return max
+    
+    elif pooling_strategy == 'mean':
+        _validate_attention_mask(attention_mask)
+        # the unsqueeze is to make the attention mask have the same shape as the embeddings
+        # the multiplication is to zero out the embeddings of the padding tokens
+        sum = torch.sum(embeddings * attention_mask.unsqueeze(-1), dim=1)
+        mean = sum / attention_mask.sum(dim=1).unsqueeze(1)
+        return mean
+    
+    else:
+        # `cls_avg`, `cls_max`, `last`, `avg`, `mean`, `max`, `all`, int]'
+        raise NotImplementedError(
+            'please specify pooling_strategy from [`cls`, `eos`, `max`, `mean`]')
